9729ba6b1ef3529c08d7d00060091cf920a56841
Asha Sanford <sanford.asha97@yahoo.com>
2021-05-03 23:55:22 -0400

    set up inquirer choices

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const inquirer = require('inquirer');
 
+// inquirer.prompt([
+//     {
+//         type: 'input',
+//         name: 'test',
+//         message: 'Type in a test code'
+//     }
+// ]).then( function(response) {
+//     console.log(response.test);
+// })
+
 inquirer.prompt([
     {
-        type: 'input',
-        name: 'test',
-        message: 'Type in a test code'
+        type: 'list',
+        name:'mainMenu',
+        message:'What would you like to do?',
+        choices: ['View All Employees','View all Employees by Department','View All Roles', 'Add Employee', 'Remove Employee','Update Employee Role','Add Deparment', 'Add Role']
     }
 ]).then( function(response) {
-    console.log(response.test);
+    console.log("Choice accepted");
 })
